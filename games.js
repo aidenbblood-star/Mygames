8632684a6da25dd87b35aae8c8daefd136ff6dd4
aidenbblood-star <aiden.b.blood@gmail.com>
2026-02-08 11:16:09 -0700

    Update games.js

--- games.js
+++ games.js
@@ -5,7 +5,8 @@ const GAMES_FOLDER = 'games';
 async function loadGames() {
     try {
         // THIS IS THE CORRECT URL TO AUTOMATICALLY GET YOUR LIST OF GAMES
-        const response = await fetch(`https://api.github.com{GITHUB_USERNAME}/${REPO_NAME}/contents/${GAMES_FOLDER}`);
+       const response = await fetch(`https://api.github.com{GITHUB_USERNAME}/${REPO_NAME}/contents/${GAMES_FOLDER}`);
+
 
         
         // Check for rate limit or folder name issues
